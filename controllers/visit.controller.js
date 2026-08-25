c9edcdc7c5588bec09845cd85084eacde0d2f6dc
WA-2211 <walaaalghanmi@gmail.com>
2026-08-25 22:06:31 +0300

    update

--- controllers/visit.controller.js
+++ controllers/visit.controller.js
@@ -18,13 +18,34 @@ async function createVisit(req, res) {
         const { place } = req.body
         const foundPlace = await Place.findById(place)
         
+        if(!foundPlace){
+            return res.status(404).json({message: 'Place not found!'})
+        }
+
         const visitedAt = new Date()
         const daysOfCoolDown = 15
         const coolDownUntil = new Date(visitedAt)
         coolDownUntil.setDate(coolDownUntil.getDate()+ daysOfCoolDown)
 
-        if(!foundPlace){
-            return res.status(404).json({message: 'Place not found!'})
+        const coolDown = await Visit.findOne({
+            user: req.user._id,
+            place,
+            coolDownUntil: {$gt: visitedAt}
+        })
+
+        const options = {
+            timeZone: 'Asia/Bahrain',
+            weekday: 'long',
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+            hour: 'numeric',
+            minute: '2-digit'
+
+        }
+
+        if(coolDown){
+            return res.status(400).json({message: `Active cooldown on this place until ${coolDown.coolDownUntil.toLocaleString('en-BH', options)}`})
         }
 
 
@@ -48,7 +69,7 @@ async function createVisit(req, res) {
 
 async function getOneVisit(req, res){
     try {
-        const getVisit = await Visit.findById(req.params.visitId)
+        const getVisit = await Visit.findById(req.params.visitId).populate('place')
         if(!getVisit){
             return res.status(404).json({message: 'Visit not found!'})
         }
@@ -66,16 +87,16 @@ async function getOneVisit(req, res){
 
 async function deleteVisit(req, res){
     try {
-        const deletedVisit = await Visit.findByIdAndDelete(req.params.visitId)
-        if(!deletedVisit){
+        const getVisit = await Visit.findById(req.params.visitId)
+        if(!getVisit){
             return res.status(404).json({message: 'Visit not found!'})
         }
-
-        if(deletedVisit.user.toString() !== req.user._id.toString()){
+        if(getVisit.user.toString() !== req.user._id.toString()){
             return res.status(403).json({message: 'Unathorized, Owner access only!'})
         }
 
-        res.status(204).json({message: 'Visit has been deleted!'})
+        const deletedVisit = await Visit.findByIdAndDelete(req.params.visitId)
+        res.status(204).json(deletedVisit)
     } catch (err) {
         res.status(500).json({ message: err.message })
 
