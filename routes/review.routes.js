88641b57879b9003ddc12855089f22528db74e85
WA-2211 <walaaalghanmi@gmail.com>
2026-08-29 17:27:08 +0300

    fix: remove if no favorites exist condition

--- routes/review.routes.js
+++ routes/review.routes.js
@@ -3,7 +3,7 @@ const reviewController = require('../controllers/review.controller')
 const validateObjectId = require('../middleware/validateObjectId')
 const verifyToken = require('../middleware/verifyToken')
 
-router.get('/', verifyToken, reviewController.getAllReviews)
+router.get('/',  reviewController.getAllReviews)
 router.post('/', verifyToken, reviewController.createReview)
 router.delete('/:reviewId', verifyToken, validateObjectId, reviewController.deleteReview)
 module.exports = router
